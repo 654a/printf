c720e8f759264495f3344cd609d71698f0267b09
Chidera Akubude <akubudechyboi00@gmail.com>
2022-08-08 22:08:03 +0100

    Partner 1

--- utils.c
+++ utils.c
@@ -7,11 +7,10 @@
  */
 
 int is_printable(char c)
-
 {
-if (c >= 32 && c < 127)
-return (1);
-return (0);
+	if (c >= 32 && c < 127)
+		return (1);
+	return (0);
 }
 
 /**
@@ -23,18 +22,19 @@ return (0);
  */
 
 int append_hexa_code(char ascii_code, char buffer[], int i)
-
 {
-char map_to[] = "0123456789ABCDEF";
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-/* The hexa format code is always 2 digits long */
-if (ascii_code < 0)
-ascii_code *= -1;
-buffer[i++] = '\\';
-buffer[i++] = 'x';
-buffer[i++] = map_to[ascii_code / 16];
-buffer[i] = map_to[ascii_code % 16];
-return (3);
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
 }
 
 /**
@@ -44,11 +44,10 @@ return (3);
  */
 
 int is_digit(char c)
-
 {
-if (c >= '0' && c <= '9')
-return (1);
-return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
 
 /**
@@ -59,13 +58,12 @@ return (0);
  */
 
 long int convert_size_number(long int num, int size)
-
 {
-if (size == S_LONG)
-return (num);
-else if (size == S_SHORT)
-return ((short)num);
-return ((int)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+	return ((int)num);
 }
 
 /**
@@ -76,11 +74,10 @@ return ((int)num);
  */
 
 long int convert_size_unsgnd(unsigned long int num, int size)
-
 {
-if (size == S_LONG)
-return (num);
-else if (size == S_SHORT)
-return ((unsigned short)num);
-return ((unsigned int)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+	return ((unsigned int)num);
 }
